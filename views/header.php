23e6b52f7a38a48e3f7c14470718e616a00506c8
Huong5925 <26a4041686@hvnh.edu.vn>
2026-04-23 21:43:09 +0700

    sua anh

--- views/header.php
+++ views/header.php
@@ -23,9 +23,7 @@ $current_page = isset($_GET['page']) ? $_GET['page'] : 'home';
 </head>
 
 <body>
-    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-        <div class="spinner-grow text-primary" role="status"></div>
-    </div>
+   
 
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
         <a href="index.php?page=home" class="navbar-brand ms-4 ms-lg-0">
