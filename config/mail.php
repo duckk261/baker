213a9096403e96d6a1c00ea0e7a5b3c61e134908
duckk261 <anhduclcvn2@gmail.com>
2026-05-07 21:46:17 +0700

    xoa tiep
    xoa tiep

--- config/mail.php
+++ config/mail.php
@@ -3,10 +3,10 @@ return [
     'driver' => 'smtp',
     'host' => 'smtp.gmail.com',
     'port' => 587,
-    'username' => 'hoangquytuyen25@gmail.com', // Thay bằng email Gmail của bạn
-    'password' => 'jxvn pjaf pckh wsjq', // Tạo app password từ Google Account
+    'username' => 'hoangquytuyen25@gmail.com',
+    'password' => 'jxvn pjaf pckh wsjq',
     'encryption' => 'tls',
-    'from_email' => 'hoangquytuyen25@gmail.com', // Thay bằng email Gmail của bạn
+    'from_email' => 'hoangquytuyen25@gmail.com', 
     'from_name' => 'Baker Store',
 ];
 ?>
